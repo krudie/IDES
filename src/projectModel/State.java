c82215a595b9a57e3861ef386c76700cce6eef61
Axel G. Michelsen <agmi02>
2005-09-23 16:35:03 +0000

    *** empty log message ***

--- src/projectModel/State.java
+++ src/projectModel/State.java
@@ -1,5 +1,38 @@
 package projectModel;
 
+import java.util.*;
+
+/**
+ * 
+ * @author Axel Gottlieb Michelsen
+ *
+ */
 public class State extends AutomatonElement{
+	private LinkedList sourceT, targetT;
+
+	public State(){
+		sourceT = new LinkedList<Transition>();
+		targetT = new LinkedList<Transition>();
+	}
+	
+	public void addSourceTransition(Transition t){
+		sourceT.add(t);
+	}
+	public void removeSourceTransition(Transition t){
+		sourceT.remove(t);
+	}
+	public ListIterator<Transition> getSourceTransitionsListIterator(){
+		return sourceT.listIterator();
+	}
+	
+	public void addTargetTransition(Transition t){
+		targetT.add(t);
+	}
+	public void removeTargetTransition(Transition t){
+		targetT.remove(t);
+	}
+	public ListIterator<Transition> getTargetTransitionListIterator(){
+		return targetT.listIterator();
+	}
 
 }
--- src/projectModel/Transition.java
+++ src/projectModel/Transition.java
@@ -1,5 +1,43 @@
 package projectModel;
 
+/**
+ * 
+ * @author Axel Gottlieb Michelsen
+ *
+ */
 public class Transition extends AutomatonElement{
+	private State sourceS, targetS;
+	private Event e;
+	
+	public Transition(State sourceS, State targetS){
+		this.sourceS = sourceS;
+		this.targetS = targetS;
+	}
 
+	public Transition(State sourceS, State targetS, Event e){
+		this.sourceS = sourceS;
+		this.targetS = targetS;
+		this.e = e;
+	}
+
+	public void setSourceState(State s){
+		this.sourceS = s;
+	}
+	public State getSourceState(){
+		return this.sourceS;
+	}
+	
+	public void setTargetState(State s){
+		this.targetS = s;
+	}
+	public State getTargetState(State s){
+		return this.targetS;
+	}
+	
+	public void setEvent(Event e){
+		this.e = e;
+	}
+	public Event getEvent(){
+		return e;
+	}
 }
