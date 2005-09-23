298e506cbe8866c1547554acc19d1fe33894dfe4
Axel G. Michelsen <agmi02>
2005-09-23 14:18:36 +0000

    *** empty log message ***

--- src/projectModel/AutomatonElement.java
+++ src/projectModel/AutomatonElement.java
@@ -2,29 +2,36 @@ package projectModel;
 
 import java.util.*;
 
+/**
+ * @author Axel Gottlieb Michelsen
+ * 
+ * This class is the superclass of elements in an automaton (suprise suprise)
+ * At the moment it holds functionality for manipulating attributes of
+ * an element, e.g. observable, controlable. 
+ * (Just make sure you type the names of the attributes in the same every time.)
+ *
+ */
 public class AutomatonElement {
 	private Hashtable attributeList;
-	
 	public AutomatonElement(){
 		attributeList = new Hashtable();
 	}
-	
 	public Object getAttribute(String aName) throws NullPointerException{
 		return attributeList.get(aName);
 	}
-	
 	public void setAttribute(String aName, Object attr) throws NullPointerException{
 		attributeList.put(aName, attr);
 	}
-	
 	public void removeAttribute(String aName) throws NullPointerException{
 		attributeList.remove(aName);
-	}
-	
+	}	
 	public void hasAttribute(String aName){
 		attributeList.containsKey(aName);
 	}
 	
+	/**
+	 * Test
+	 */
 	public static void main(String args[]){
 		AutomatonElement ae = new AutomatonElement();
 		ae.setAttribute("integer", new Integer(42));
