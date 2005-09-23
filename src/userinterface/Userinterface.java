dbc00d812fdb49b4d94f957ff8fc33c9116e3c4c
Kristian Edlund <edlund>
2005-09-23 12:55:16 +0000

    *** empty log message ***

--- src/userinterface/Userinterface.java
+++ src/userinterface/Userinterface.java
@@ -1,7 +1,8 @@
 package userinterface;
 
-import org.eclipse.swt.widgets.*;
 import org.eclipse.swt.*;
+import org.eclipse.swt.graphics.*;
+import org.eclipse.swt.widgets.*;
 
 public class Userinterface {
 
@@ -27,7 +28,16 @@ public class Userinterface {
 			shell = new Shell(display, SWT.SHELL_TRIM);
 			shell.setText("IDES2");
 			shell.setSize(800,620);
-			shell.layout();
+			CoolBar bar = new CoolBar (shell, SWT.BORDER);
+			for (int i=0; i<2; i++) {
+				CoolItem item = new CoolItem (bar, SWT.NONE);
+				Button button = new Button (bar, SWT.PUSH);
+				button.setText ("Button " + i);
+				Point size = button.computeSize (SWT.DEFAULT, SWT.DEFAULT);
+				item.setPreferredSize (item.computeSize (size.x, size.y));
+				item.setControl (button);
+			}
+			bar.pack ();
 			shell.open();
 			splash.dispose();
 			while (!shell.isDisposed()) { if (!display.readAndDispatch()) display.sleep(); }
