5f14a44bb8a649084141b6cd8f8fe325a60c7f0a
Kristian Edlund <edlund>
2005-10-19 15:48:28 +0000

    *** empty log message ***

---
+++ src/projectPresentation/Layouter.java
@@ -0,0 +1,11 @@
+package projectPresentation;
+
+public class Layouter{
+
+    public static final String WINDOWS_COMMAND = "localDotEngine\\Win32\\dot -Tdot";
+    public static final String LINUX_COMMAND = "localDotEngine/Linux/dot -Tdot";
+    public static final String SOLARIS_COMMAND = "localDotEngine/Solaris/dot -Tdot";   
+    
+ 
+    
+}
