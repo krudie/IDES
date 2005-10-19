b0d4a47386e177b61b4348aa93be88c7b82083d8
Axel G. Michelsen <agmi02>
2005-10-19 14:14:47 +0000

    *** empty log message ***

--- src/projectModel/Automaton.java
+++ src/projectModel/Automaton.java
@@ -17,7 +17,7 @@ public class Automaton implements Cloneable{
 
     private String name = null;
 
-    public Automaton(String name) {
+    public Automaton(String name){
         states = new LinkedList<State>();
         transitions = new LinkedList<Transition>();
         events = new LinkedList<Event>();
@@ -38,26 +38,29 @@ public class Automaton implements Cloneable{
         while(ti.hasNext()){
             Transition oldt = ti.next();
             State source = clone.getState(oldt.getSource().getId());
-            State target = clone.getState(oldt.getTarget().getId()); 
-            Event event = clone.getEvent(oldt.getEvent().getId());
-            clone.add(new Transition(oldt, source, target, event));
-        }        
+            State target = clone.getState(oldt.getTarget().getId());
+            if(oldt.getEvent() == null) clone.add(new Transition(oldt, source, target));
+            else{
+                Event event = clone.getEvent(oldt.getEvent().getId());
+                clone.add(new Transition(oldt, source, target, event));
+            }
+        }
         return clone;
     }
-    
-    public String getName() {
+
+    public String getName(){
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name){
         this.name = name;
     }
 
-    public void add(State s) {
+    public void add(State s){
         states.add(s);
     }
 
-    public void remove(State s) {
+    public void remove(State s){
         states.remove(s);
         ListIterator<Transition> sources = s.getSourceTransitionsListIterator();
         while(sources.hasNext()){
@@ -69,75 +72,73 @@ public class Automaton implements Cloneable{
         }
     }
 
-    public ListIterator<State> getStateIterator() {
+    public ListIterator<State> getStateIterator(){
         return states.listIterator();
     }
 
-    public State getState(int id) {
+    public State getState(int id){
         ListIterator<State> si = states.listIterator();
-        while (si.hasNext()) {
+        while(si.hasNext()){
             State s = si.next();
-            if (s.getId() == id)
-                return s;
+            if(s.getId() == id) return s;
         }
         return null;
     }
 
-    public void add(Transition t) {
+    public void add(Transition t){
         t.getSource().addSourceTransition(t);
         t.getTarget().addTargetTransition(t);
         transitions.add(t);
     }
 
-    public void remove(Transition t) {
+    public void remove(Transition t){
         t.getSource().removeSourceTransition(t);
         t.getTarget().removeTargetTransition(t);
         transitions.remove(t);
     }
 
-    public ListIterator<Transition> getTransitionIterator() {
+    public ListIterator<Transition> getTransitionIterator(){
         return transitions.listIterator();
     }
 
-    public void add(Event e) {
+    public void add(Event e){
         events.add(e);
     }
 
-    public void remove(Event e) {
+    public void remove(Event e){
         events.remove(e);
     }
 
-    public ListIterator<Event> getEventIterator() {
+    public ListIterator<Event> getEventIterator(){
         return events.listIterator();
     }
 
-    public Event getEvent(int id) {
+    public Event getEvent(int id){
         ListIterator<Event> ei = events.listIterator();
-        while (ei.hasNext()) {
+        while(ei.hasNext()){
             Event e = ei.next();
-            if (e.getId() == id)
-                return e;
+            if(e.getId() == id) return e;
         }
         return null;
     }
 
-    public void toXML(PrintStream ps) {
+    public void toXML(PrintStream ps){
         ps.println("<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>");
         ps.println("<automaton>");
         Iterator<State> si = states.iterator();
-        while (si.hasNext()) {
+        while(si.hasNext()){
             si.next().toXML(ps, "  ");
         }
 
         Iterator<Event> ei = events.iterator();
-        while (ei.hasNext()) {
+        while(ei.hasNext()){
             ei.next().toXML(ps, "  ");
         }
 
         Iterator<Transition> ti = transitions.iterator();
-        while (ti.hasNext()) {
+        while(ti.hasNext()){
             ti.next().toXML(ps, "  ");
         }
         ps.println("</automaton>");
-    }   
+    }
 }
